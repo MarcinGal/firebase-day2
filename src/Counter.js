59e083182b492e1826450dff859ce4ca40e9cf11
marcingal <marcin921125@gmail.com>
2018-11-28 11:42:18 +0100

    saving state to firebase

--- src/Counter.js
+++ src/Counter.js
@@ -7,15 +7,27 @@ class Counter extends React.Component {
     }
 
     incHandler = () => this.setState({ number: this.state.number + 1 })
-
     decHandler = () => this.setState({ number: this.state.number - 1 })
+
+    saveToFirebase = () => {
+        fetch(
+            'https://poniedzialek-ee614.firebaseio.com/counter.json',
+            {
+                method: 'PUT',
+                body: JSON.stringify(this.state.number)
+            }
+        )
+    }
+
+    componentDidUpdate(){
+        this.saveToFirebase()
+    }
 
     render() {
         return (
             <div>
                 <h2>{this.state.number}</h2>
-                <button
-                    onClick={this.incHandler}>+</button>
+                <button onClick={this.incHandler}>+</button>
                 <button onClick={this.decHandler}>-</button>
             </div>
         )
