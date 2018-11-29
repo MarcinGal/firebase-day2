e0b16cc4664c526c764b614812539d0a6aacfd26
marcingal <marcin921125@gmail.com>
2018-11-29 10:05:54 +0100

    changing object into array

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import NewMessageForm from './NewMessageForm'
 import { database } from '../firebaseConfig'
-
+import { mapObjectToArray } from '../utils'
 const dbMessages = database.ref('/chat')
 
 class Chat extends React.Component {
     state = {
         newMessageText: 'krowa',
         messages: []
+    }
+
+
+    onNewMessageTextChangeHandler = event => this.setState({ newMessageText: event.target.value })
+
+    componentDidMount() {
+        dbMessages.on(
+            'value',
+            snapshot => console.log(mapObjectToArray(snapshot.val()))
+        )
     }
 
 
